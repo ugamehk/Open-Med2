5e5ea5fd8b7e3b7f66ae736b1473bffb957b8c20
ugamehk <Diu120pk>
2017-11-23 00:31:12 +0800

    Alarm in progress

---
+++ app/src/main/java/com/example/yin/openmed/AlarmReceiver.java
@@ -0,0 +1,18 @@
+package com.example.yin.openmed;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        Toast.makeText(context, "ALARM", Toast.LENGTH_LONG).show();
+    }
+}
+
+public class AlarmReceiver extends BroadcastReceiver {
+}
--- app/src/main/java/com/example/yin/openmed/Home.java
+++ app/src/main/java/com/example/yin/openmed/Home.java
@@ -1,13 +1,22 @@
 package com.example.yin.openmed;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.Calendar;
 
 public class Home extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //test4
         setContentView(R.layout.activity_home);
+        AlarmManager alarmManager = (AlarmManager)  getSystemService(ALARM_SERVICE);
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.SECOND,5);
+
+        PendingIntent.getBroadcast(this, 100, new Intent(""))
     }
 }
