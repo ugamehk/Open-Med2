79e82e942412be139b6cb007a7b38f0e27875a39
ugamehk <Diu120pk>
2017-11-22 20:46:26 +0800

    comment2

--- app/src/main/java/com/example/yin/openmed/Home.java
+++ app/src/main/java/com/example/yin/openmed/Home.java
@@ -8,6 +8,7 @@ public class Home extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //test3
         setContentView(R.layout.activity_home);
     }
 }
